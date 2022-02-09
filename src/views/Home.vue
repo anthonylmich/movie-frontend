b061d049897af3cd0153a7882b17880851b4ce41
Anthony <tonymich2012@gmail.com>
2022-02-09 01:33:09 -0800

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      movies: [],
+    };
+  },
+  created: function () {
+    this.indexMovies();
+  },
+  methods: {
+    indexMovies: function () {
+      console.log("hello");
+      axios.get("https://localhost:3000/movies").then((response) => {
+        console.log(response.data);
+        this.movies = response.data;
+      });
+    },
   },
 };
 </script>
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+  </div>
+  <div>
+    <p>Sanity check</p>
+  </div>
+  <div v-for="movie in movies" v-bind:key="movie.id">
+  <h2>Movie: {{ movie.title }}</h2>
+  <p>Plot: {{ movie.plot }}</p>
+  </div>
+  <button v-on:click="indexMovies()">Index</button>
+</template>
+
+<style></style>
